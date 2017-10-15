9d01ee63e1f28cdab7b2a21683d91a21f4f52464
kevin <willardquing@gmail.com>
2017-10-15 23:38:13 +0800

    upload control

--- src/uploadInput/uploadInput.jsx
+++ src/uploadInput/uploadInput.jsx
@@ -3,6 +3,36 @@ import React, { Component } from 'react';
 class UploadInput extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            files: {}
+        }
+        
+        this.selectFile = this.selectFile.bind(this)
+        this.upload = this.upload.bind(this)
+    }
+
+    upload() {
+        let data = new FormData();
+        data.append("file", this.state.files[0])
+
+        fetch(`http://localhost:3000/upload`,
+            {
+                method: "POST",
+                // headers: {
+                //     "Content-type": "image"
+                // },
+                // body: this.state.files[0]
+                body: data
+            })
+    }
+
+    selectFile(a) {
+        // console.log(a)
+        // console.log(a.target.files)
+        this.setState({
+                files: a.target.files
+            })
+
     }
 
     render() {
@@ -10,10 +40,10 @@ class UploadInput extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <input type="file" />
+                        <input type="file" onChange={this.selectFile}/>
                     </div>
                     <div className="col">
-                        <button type="submit">Upload</button>
+                        <button type="submit" onClick={this.upload}>Upload</button>
                     </div>
                 </div>
             </div>);
